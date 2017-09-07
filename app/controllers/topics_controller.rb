749cc2b1747ee4d8594e48debed6acbc1fde7327
qiaoyu <qiaoyu1111358@gmail.com>
2017-09-07 18:38:05 +0800

    add

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -23,26 +23,27 @@ class TopicsController < ApplicationController
 
   # POST /topics
   # POST /topics.json
-  def create
-    @topic = Topic.new(topic_params)
 
-    respond_to do |format|
-      if @topic.save
-        format.html { redirect_to @topic, notice: 'Topic was successfully created.' }
-        format.json { render :show, status: :created, location: @topic }
-      else
-        format.html { render :new }
-        format.json { render json: @topic.errors, status: :unprocessable_entity }
-      end
+  def create
+  @topic = Topic.new(topic_params)
+
+  respond_to do |format|
+    if @topic.save
+      format.html { redirect_to topics_path, notice: 'Topic was successfully created.' }
+      format.json { render :show, status: :created, location: @topic }
+    else
+      format.html { render :new }
+      format.json { render json: @topic.errors, status: :unprocessable_entity }
     end
   end
+end
 
   # PATCH/PUT /topics/1
   # PATCH/PUT /topics/1.json
   def update
     respond_to do |format|
       if @topic.update(topic_params)
-        format.html { redirect_to @topic, notice: 'Topic was successfully updated.' }
+        format.html { redirect_to topics_path, notice: 'Topic was successfully updated.' }
         format.json { render :show, status: :ok, location: @topic }
       else
         format.html { render :edit }
